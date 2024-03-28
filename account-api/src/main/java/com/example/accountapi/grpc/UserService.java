e5117f8c2062b20d26b7fc568ab13cb8430a5f23
ScottSung7 <sungscott@naver.com>
2024-03-28 20:39:50 +0900

    [018 CI/CD] gRPC Disabled. 유레카 등록 이슈로 gRPC 임시 주석처리

--- account-api/src/main/java/com/example/accountapi/grpc/UserService.java
+++ account-api/src/main/java/com/example/accountapi/grpc/UserService.java
@@ -1,27 +1,27 @@
-package com.example.accountapi.grpc;
-
-import com.example.user.UserInformation;
-import com.example.user.UserInformationRequest;
-import com.example.user.UserServiceGrpc;
-import io.grpc.stub.StreamObserver;
-import net.devh.boot.grpc.server.service.GrpcService;
-
-
-@GrpcService
-public class UserService extends UserServiceGrpc.UserServiceImplBase {
-    @Override
-    public void getUserInformation(UserInformationRequest request, StreamObserver<UserInformation> responseObserver) {
-
-        System.out.println("Request received");
-        System.out.println(request.getUserId());
-
-        UserInformation userInformation = UserInformation.newBuilder()
-                .setUserId(request.getUserId())
-                .setName("hi")
-                .build();
-
-        responseObserver.onNext(userInformation);
-        responseObserver.onCompleted();
-
-    }
-}
+//package com.example.accountapi.grpc;
+//
+//import com.example.user.UserInformation;
+//import com.example.user.UserInformationRequest;
+//import com.example.user.UserServiceGrpc;
+//import io.grpc.stub.StreamObserver;
+//import net.devh.boot.grpc.server.service.GrpcService;
+//
+//
+//@GrpcService
+//public class UserService extends UserServiceGrpc.UserServiceImplBase {
+//    @Override
+//    public void getUserInformation(UserInformationRequest request, StreamObserver<UserInformation> responseObserver) {
+//
+//        System.out.println("Request received");
+//        System.out.println(request.getUserId());
+//
+//        UserInformation userInformation = UserInformation.newBuilder()
+//                .setUserId(request.getUserId())
+//                .setName("hi")
+//                .build();
+//
+//        responseObserver.onNext(userInformation);
+//        responseObserver.onCompleted();
+//
+//    }
+//}
--- order-api/src/main/java/com/example/orderapi/gRPC/Config.java
+++ order-api/src/main/java/com/example/orderapi/gRPC/Config.java
@@ -1,18 +1,18 @@
-package com.example.orderapi.gRPC;
-
-import com.google.protobuf.util.JsonFormat;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.protobuf.ProtobufJsonFormatHttpMessageConverter;
-
-@Configuration
-public class Config {
-
-    @Bean
-    public ProtobufJsonFormatHttpMessageConverter protobufJsonFormatHttpMessageConverter(){
-        return new ProtobufJsonFormatHttpMessageConverter(
-                JsonFormat.parser().ignoringUnknownFields(),
-                JsonFormat.printer().omittingInsignificantWhitespace().includingDefaultValueFields()
-        );
-    }
-}
+//package com.example.orderapi.gRPC;
+//
+//import com.google.protobuf.util.JsonFormat;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.converter.protobuf.ProtobufJsonFormatHttpMessageConverter;
+//
+//@Configuration
+//public class Config {
+//
+//    @Bean
+//    public ProtobufJsonFormatHttpMessageConverter protobufJsonFormatHttpMessageConverter(){
+//        return new ProtobufJsonFormatHttpMessageConverter(
+//                JsonFormat.parser().ignoringUnknownFields(),
+//                JsonFormat.printer().omittingInsignificantWhitespace().includingDefaultValueFields()
+//        );
+//    }
+//}
